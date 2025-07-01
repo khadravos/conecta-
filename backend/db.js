fbf7bc7bd7b3b32f956d7c49e1711352f53d9f5d
Messias <messiaslucio28@gmail.com>
2025-07-01 13:55:28 -0300

    Atualiza projeto com nova versão

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,18 @@
-import mysql from "mysql2";
-import fs from "fs";
-import path from "path";
-// Caminho para o certificado SSL (opcional se fornecido)
-const sslOptions = {
-    ca: fs.readFileSync(path.resolve("certs/ca.pem")),
-    rejectUnauthorized: true, // ative isso se quiser validar certificado
-};
+import mysql from 'mysql2';
+import fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const db = mysql.createConnection({
-  host: "conecta-messiaslucio28-2dbb.i.aivencloud.com",
-  port: 27923,
-  user: "avnadmin",
-  password: "AVNS_Pc91gUOvb8ow82_dykB",
-  database: "defaultdb",
-  ssl: sslOptions, // Ativa SSL (mesmo sem certificado, necessário para Aiven)
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(path.resolve("certs/ca.pem")),
+    rejectUnauthorized: true
+  }
 });
