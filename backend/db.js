e78dd915223bc81674528b71aab9014c21c3d880
Messias <messiaslucio28@gmail.com>
2025-07-01 13:45:48 -0300

    Atualiza projeto com nova versão

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,17 @@
-import mysql from "mysql"
+import mysql from "mysql2";
+import fs from "fs";
+import path from "path";
+// Caminho para o certificado SSL (opcional se fornecido)
+const sslOptions = {
+    ca: fs.readFileSync(path.resolve("certs/ca.pem")),
+    rejectUnauthorized: true, // ative isso se quiser validar certificado
+};
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "28DE_maio",
-    database: "Conecta"
-})
+  host: "conecta-messiaslucio28-2dbb.i.aivencloud.com",
+  port: 27923,
+  user: "avnadmin",
+  password: "AVNS_Pc91gUOvb8ow82_dykB",
+  database: "defaultdb",
+  ssl: sslOptions, // Ativa SSL (mesmo sem certificado, necessário para Aiven)
+});
